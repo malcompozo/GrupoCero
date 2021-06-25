d2bf80bc893edf0c9b0b89d8cfda249e50667252
nicolascberna <nicolascberna@gmail.com>
2021-06-25 11:45:22 -0400

    detalle artista listo

--- GrupoCero/artista/urls.py
+++ GrupoCero/artista/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
 
     path('', views.artistas, name='artistas'),
-    path('detalle_artista/', views.detalle_artista, name='detalle_artista'),
+    path('detalle_artista/<int:artista_id>', views.detalle_artista, name='detalle_artista'),
     path('detalle_obra/', views.detalle_obra, name='detalle_obra'),
     path('editar_perfil/', views.editar_perfil, name='editar_perfil'),
     path('gestion_obras/', views.gestion_obras, name='gestion_obras'),
