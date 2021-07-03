7a0fe2de5f0482cadcdf04aca375cb633f4d9768
malcom <malcompozo@gmail.com>
2021-07-03 10:35:21 -0400

    api crud

--- GrupoCero/rest_obra/views.py
+++ GrupoCero/rest_obra/views.py
@@ -1,5 +1,7 @@
+import re
 from django.shortcuts import render
 from rest_framework import serializers, status
+from rest_framework import response
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.parsers import JSONParser
@@ -20,10 +22,33 @@ def lista_obras(request):
         serializer = ObraSerializer(obra, many=True)
         return Response(serializer.data)
     elif request.method == 'POST':
-        data = JSONParser.parse(request)
+        data = JSONParser().parse(request)
         serializer = ObraSerializer(data=data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET','PUT','DELETE'])    
+def detalle_obra(request, publicacion_id):
+    try:
+        obra = Publicacion.objects.get(id = publicacion_id)
+    except Publicacion.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    if request.method == 'GET':
+        serializer = ObraSerializer(obra)
+        return Response(serializer.data)
+    if request.method == 'PUT':
+        data = JSONParser().parse(request)
+        serializer = ObraSerializer(obra, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'DELETE':
+        obra.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+        
+        
